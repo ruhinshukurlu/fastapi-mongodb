5fe291d065ab97e798ef7301f869d34d2631815a
Ruhin Shukurlu <ruhinshukurlu@gmail.com>
2023-03-31 16:30:45 +0400

    mongodb connected

---
+++ app/database.py
@@ -0,0 +1,15 @@
+from pymongo import mongo_client
+import pymongo
+from app.config import settings
+
+
+client = mongo_client.MongoClient(settings.DATABASE_URL)
+print("Connected to Mongodb ....")
+
+
+db = client[settings.MONGO_INITDB_DATABASE]
+User = db.users
+Post = db.posts
+
+User.create_index([("email", pymongo.ASCENDING)], unique=True)
+Post.create_index([("title", pymongo.ASCENDING)], unique=True)
